49c9dc9ac1e6e70c3319fb723a6c19f45b5e693f
iphayao <phayaob@gmail.com>
2018-08-30 21:21:21 +0700

    Adding Some Validation

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -8,11 +8,18 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def new
+        @article = Article.new
+    end
+
     def create
         @article = Article.new(article_params)
 
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
     private
@@ -20,7 +27,6 @@ class ArticlesController < ApplicationController
             params.require(:article).permit(:title, :text);
         end
 
-    def new
-    end
+
 
 end
